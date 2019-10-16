c4098fb0df2ca14fd92eaa9e8e110d3bdaa614e8
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 21:52:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,10 +35,10 @@ class Student
   
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
+      INSERT INTO students (name, grade)
+        VALUES (?, ?)
     SQL
- 
+
     DB[:conn].execute(sql, self.name, self.grade)
   end 
   
