0c94e0c0bfb7f0d047b6a1cd91404579aeb1d803
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 21:40:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,11 +4,12 @@ class Student
   #  with DB[:conn]  
   
   
-  
+  attr_accessor :name, :grade
   attr_reader :id 
   
-  def initialize()
-    
+  def initialize(name, grade)
+    @name = name 
+    @grade = grade
   end 
   
   
