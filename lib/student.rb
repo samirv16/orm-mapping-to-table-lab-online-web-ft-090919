e784e0ba2cda27ab81cbe7931ab8f20f8109f2d7
edras vallecillo <samir.vallecillo@outlook.com>
2019-10-16 21:44:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,13 @@ class Student
   end 
   
   def self.create_table
-    
+    sql =  <<-SQL 
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        album TEXT
+        )
+        SQL
   end 
   
 end
